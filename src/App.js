f1ff5beb5acce86156189a137bf62a5e0253540a
DucPhu <ducphu.170720@gmail.com>
2022-03-15 08:55:08 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react'
+import './App.css';
+
+
+function App(props) {
+    
+    return (
+        <>
+        </>
+        
+    );
+}
+
+export default App;
---
+++ src/template/Layout/Header.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Component, useState } from 'react';
+import "../../style/Layout/header.scss";
+
+function Header(props:any) {
+
+  // variable
+  const {active} = props
+// function
+  const {changeActiveHam} = props
+
+  
+    return (
+      <header id='header' >
+       
+      </header>
+    );
+  }
+  
+  export default Header;
