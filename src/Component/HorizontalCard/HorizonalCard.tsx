09b7831c2462a21befb6ae13007e43f1dbfd8aa7
DucPhu <ducphu.170720@gmail.com>
2022-03-15 10:49:35 +0700

    phu add images

---
+++ src/Component/HorizontalCard/HorizonalCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HorizontalCard.scss';
+
+
+
+function HorizontalCard(props:any) {
+    
+    return (
+        <div className='block-horizon_card'>
+            
+        </div>
+    );
+}
+
+export default HorizontalCard;
