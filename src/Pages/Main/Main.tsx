5828b2ee22dd4b57c86991d353575bfa8bfd78af
DucPhu <ducphu.170720@gmail.com>
2022-03-15 10:25:51 +0700

    vex add source

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,15 +1,9 @@
 
-import { gsap } from 'gsap';
-import { ScrollToPlugin } from 'gsap/all';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import React from 'react';
 
-import ReactDOM from 'react-dom';
-import { useState, useEffect } from 'react'
+import React from 'react';
 import './Main.scss';
 
-import Header from '../../Component/Header/Header';
-import Footer from '../../Component/Footer/Footer';
+
 
 function App(props:any) {
     
