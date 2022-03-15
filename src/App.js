aba01c8fcca114ef396faa8a00993fed8b49d9a7
huynguyen52 <huynguyen5212@gmail.com>
2022-03-15 14:57:11 +0700

    Header section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import '../src/Assets/font/font.scss';
-import Main from "../src/Pages/Main/Main";
 import './App.css';
+import RouterWrapper from './Pages/Routers/RouterWrapper';
 
-
-
-function App(props) {
-    
-    return (
-        <>
-            <Main/>
-        </>
-        
-    );
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path={'/'} component={RouterWrapper} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
